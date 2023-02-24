2168713c8f2d1725d96be7f817fccd443986c5c8
Mmmccc <ch.minji.k@gmail.com>
2023-02-23 19:06:31 -0500

    Done rough visual of schedule using React Schedule It includes pop up for create/delete/modify events (double click to create and click event to delete/modify)

--- web/src/components/Schedule/Schedule.jsx
+++ web/src/components/Schedule/Schedule.jsx
@@ -3,53 +3,147 @@ import { useEffect, useState } from "react";
 import Api from "../../helpers/api";
 import Button from "../Common/Button";
 import { useAuth0 } from "@auth0/auth0-react";
+
+
+
+// SCHEDULELR IMPORTS AND ALL
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import { ViewState, IntegratedEditing, EditingState } from '@devexpress/dx-react-scheduler';
+import {
+    Scheduler,
+    ConfirmationDialog,
+    Appointments,
+    Toolbar,
+    WeekView,
+    DateNavigator,
+    AppointmentTooltip,
+    AppointmentForm,
+} from '@devexpress/dx-react-scheduler-material-ui';
+
 export default function Schedule() {
-  const { getAccessTokenSilently } = useAuth0();
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [events, setEvents] = useState([]);
+    //ORIGINAL VARIABLES FOR CREATE EVENT 
+    const { getAccessTokenSilently } = useAuth0();
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("");
 
-  const api = new Api();
+    const [events, setEvents] = useState([]);
+    const [mappedEvents, setMappedEvents] = useState([]);
 
-  useEffect(() => {
-    try {
-      getAllEvents();
-    } catch (err) {
-      console.log("error" + err);
+    const api = new Api();
+
+    useEffect(() => {
+        try {
+            getAllEvents();
+            console.log("*");
+        } catch (err) {
+            console.log("error" + err);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    async function getAllEvents() {
+        api.getEvents(await getAccessTokenSilently()).then((res) => {
+            setEvents(res.events);
+            setMappedEvents(mapEvents);    //map to another form that can be read by react scheduler
+            console.log(events);
+        });
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
-  async function getAllEvents() {
-    api.getEvents(await getAccessTokenSilently()).then((res) => {
-      setEvents(res.events);
-    });
-  }
+    //   async function createEvent(event) {    //ORIGINAL CODE TO CREATE EVENT 
+    //     event.preventDefault(); //prevent page refresh
 
-  async function createEvent(event) {
-    event.preventDefault(); //prevent page refresh
+    //     //create even with specified title and description, set start time to now and end time to 1 hour from now (can be changed once we have interface to pick those)
+    //     await api
+    //       .createEvent(
+    //         await getAccessTokenSilently(),
+    //         title,
+    //         description,
+    //         startDate,
+    //         endDate 
+    //       )
+    //       .then((res) => {
+    //         console.log(res);
+    //         setEvents(res.message);
+    //       });
 
-    //create even with specified title and description, set start time to now and end time to 1 hour from now (can be changed once we have interface to pick those)
-    await api
-      .createEvent(
-        await getAccessTokenSilently(),
-        title,
-        description,
-        new Date(Date.now()).toISOString(),
-        new Date(Date.now() + 1000000 * 60 * 60).toISOString() // 1 hour from now
-      )
-      .then((res) => {
-        console.log(res);
-        setEvents(res.message);
-      });
+    //     //refresh events
+    //     getAllEvents();
+    //     }
 
-    //refresh events
-    getAllEvents();
-  }
 
-  return (
-    <div>
-      <form onSubmit={createEvent}>
+    //IMPLEMENT CREATE/EDIT/DELETE EVENT HERE
+    const commitChanges = ({ added, changed, deleted }) => {
+        if (added) {
+            //CREATE EVENT
+            console.log("CREATED");
+        }
+        if (changed) {
+            //EDIT EVENT
+            console.log("CHANGED");
+        }
+        if (deleted !== undefined) {
+            //DELETE EVENT
+            console.log("DELETED");
+        }
+        //refresh events
+        getAllEvents();
+    };
+
+
+
+    function mapEvents() {
+        const mapped = events.map(value => ({
+            startDate: value.start_date,
+            endDate: value.end_date,
+            title: value.title,
+            id: value.id,
+            //rRule: value.isReccuring,     TODO: once create recurring event is implemented
+            //endPeriod: value.
+            //description and event_list_id are not mapped
+        }))
+        return mapped;
+    }
+
+
+    return (
+        <div>
+            <Paper>
+                <Scheduler
+                    data={mappedEvents}
+                    height={750}
+                >
+                    <EditingState
+                        onCommitChanges={commitChanges}
+                    />
+                    <ViewState
+                        defaultCurrentDate="2023-02-05"
+                    />
+                    <WeekView
+                        startDayHour={6}
+                        endDayHour={24}
+                    />
+                    <IntegratedEditing />
+                    <Toolbar />
+                    <DateNavigator />
+                    <ConfirmationDialog />
+                    <Appointments />
+                    <AppointmentTooltip
+                        showDeleteButton
+                        showOpenButton
+                    />
+                    <AppointmentForm />
+                </Scheduler>
+            </Paper>
+
+
+
+
+            {/* ORIGINAL CONTENT FROM MAIN BRANCH
+
+             <form onSubmit={createEvent}>
         <div className="flex">
           <div className="flex flex-col mb-2 mr-2">
             <label className="text-white  text-sm font-bold mb-1">Title</label>
@@ -97,7 +191,9 @@ export default function Schedule() {
               }
             </li>
           );
-        })}
-    </div>
-  );
+        })} END OF MAIN BRANCH */}
+
+
+        </div>
+    );
 }
