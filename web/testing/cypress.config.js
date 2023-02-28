307efd0d5106e5f76bc18f75e903b59943ab0d89
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2023-02-28 16:59:02 -0500

    feat(web): popup for incorrect event dates, added e2e test and correct file naming (#100)
    * feat(web): add error popup for incorrect dates
    
    * test(web): correct naming and added incorrect date test
    
    * fix(web): duplicate/wrong package install
    
    * chore(web): unused package
    
    * chore(web): again
    
    * chore(web): increase e2e timeout globally

--- web/testing/cypress.config.js
+++ web/testing/cypress.config.js
@@ -2,9 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-    },
+    setupNodeEvents(on, config) {},
   },
   videoCompression: 0,
   projectId: "1vse71",
+  defaultCommandTimeout: 12000,
 });
