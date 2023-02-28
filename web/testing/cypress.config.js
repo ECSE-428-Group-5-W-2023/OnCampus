c6f46ffd326202cdfd88dea7d194cb07dfa7bb67
PierreMasselot1 <pierrem@orohealth.me>
2023-02-27 20:39:54 -0500

    test(web): record option

--- web/testing/cypress.config.js
+++ web/testing/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
+  projectId: "1vse71",
 });
